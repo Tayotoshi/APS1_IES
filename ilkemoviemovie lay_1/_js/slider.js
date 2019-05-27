05d2a6227f9b206a7b058157097dc4af12148d24
Bernardo Souza <48145131+Tayotoshi@users.noreply.github.com>
2019-05-27 10:59:43 -0300

    Add files via upload

---
+++ ilkemoviemovie lay_1/_js/slider.js
@@ -0,0 +1,13 @@
+$('.thing1').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    });
+
+$('.thing').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    });
